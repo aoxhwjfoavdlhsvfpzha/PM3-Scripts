423ecde5166f06da2f3c399e4f742a5cb72e1db7
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2024-08-08 16:01:59 -0700

    Add files via upload

--- Windows Scripts/windows-generic.bat
+++ Windows Scripts/windows-generic.bat
@@ -1,6 +1,6 @@
 @echo off
-curl -LO "https://github.com/Gator96100/ProxSpace/releases/download/v3.11/ProxSpace.7z"
-tar -xvf "./ProxSpace.7z"
+curl -L -o "ProxSpace.tar.xz" "https://siliconbased.us/pm3/install/ProxSpace/ProxSpace_3.11.tar.xz"
+tar -xvf "./ProxSpace.tar.xz"
 cd ProxSpace
 call runme64.bat -c exit
 curl -L -o "pm3/ProxSpaceScript.sh" "https://siliconbased.us/pm3/install/scripts/ProxSpaceScript-generic.sh"
