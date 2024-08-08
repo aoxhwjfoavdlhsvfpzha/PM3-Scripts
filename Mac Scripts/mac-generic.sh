914361f84238572a6495e3e654279269b4883f70
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2024-08-08 00:20:23 -0700

    Add files via upload
    Updated Following initial testing

--- Mac Scripts/mac-generic.sh
+++ Mac Scripts/mac-generic.sh
@@ -1,58 +1,34 @@
 #!/bin/bash
 
-rerun_check="${1:-0}"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-scriptpath=$(pwd);
-script="$scriptpath"
-script+="/mac-generic.sh"
+brew install readline qt5 pkgconfig coreutils
+brew install RfidResearchGroup/proxmark3/arm-none-eabi-gcc
+brew install recode
+brew install astyle
+brew install gnu-sed
+brew install openssl
 
-script=$(echo $script | sed 's/ /\\ /g')
+#Get and enter repo:
+git clone https://github.com/RfidResearchGroup/proxmark3.git ~/proxmark3
+cd ~/proxmark3
+git pull
 
-if [ "$rerun_check" -ne "1" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    brew install readline qt5 pkgconfig coreutils
-    brew install RfidResearchGroup/proxmark3/arm-none-eabi-gcc
-    brew install recode
-    brew install astyle
-    brew install gnu-sed
-    brew install openssl
-
-    #Get and enter repo:
-    git clone https://github.com/RfidResearchGroup/proxmark3.git ~/proxmark3
-    cd ~/proxmark3
-    git pull
-
-    #Check PM3 Connection:
-    lsusb | grep -i proxmark
-    EXIT=$?
-    if [ "$EXIT" -ne "0" ]; then
-        echo "Wizard couldn't detect PM3, if it isn't connected it won't be flashed!"
-        read -p "Continue? Y/n:" PM3_detect_conf
-        if [ "$PM3_detect_conf" == "" ]; then
-            PM3_detect_conf="y"
-        fi
-        if [[ "$PM3_detect_conf" != "y" ]] && [[ "$PM3_detect_conf" != "Y" ]]; then
-            exit 1
-        fi
+#Check PM3 Connection:
+sudo dmesg | grep -i proxmark
+EXIT=$?
+if [ "$EXIT" -ne "0" ]; then
+    echo "Wizard couldn't detect PM3, if it isn't connected it won't be flashed!"
+    read -p "Continue? Y/n:" PM3_detect_conf
+    if [ "$PM3_detect_conf" == "" ]; then
+        PM3_detect_conf="y"
     fi
-
-    make accessrights
-
-    #log into new instance for permissions to take hold
-    sudo -u $USER "$script" 1
-    exit 0
+    if [[ "$PM3_detect_conf" != "y" ]] && [[ "$PM3_detect_conf" != "Y" ]]; then
+        exit 1
+    fi
 fi
 
 cd ~/proxmark3
-
-
-[ -r /dev/ttyACM0 ] && [ -w /dev/ttyACM0 ] && echo ok | grep -i ok
-EXIT=$?
-if [ "$EXIT" -ne "0" ]; then
-    echo "Error making accessrights! Aborting!"
-    exit 2
-fi
 
 cp Makefile.platform.sample Makefile.platform
 gsed -i '0,/PLATFORM=PM3RDV4/{s/PLATFORM=PM3RDV4/#PLATFORM=PM3RDV4/}' Makefile.platform
