16ef9b46c3f8524c74c9f7bf37bd2259575c8c0c
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2024-08-08 01:51:03 -0700

    Add files via upload
    Removed unnecessary install of gnu-sed

--- Mac Scripts/mac-rdv4.sh
+++ Mac Scripts/mac-rdv4.sh
@@ -6,7 +6,6 @@ brew install readline qt5 pkgconfig coreutils
 brew install RfidResearchGroup/proxmark3/arm-none-eabi-gcc
 brew install recode
 brew install astyle
-brew install gnu-sed
 brew install openssl
 
 #Get and enter repo:
