a92a1f13991119bc84a6957d615cbe09b384b3d4
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2024-08-12 01:49:15 -0700

    Add files via upload

--- Windows Scripts/windows-rdv4.bat
+++ Windows Scripts/windows-rdv4.bat
@@ -1,5 +1,5 @@
 @echo off
-curl -LC - --retry 5 --retry-max-time 0 -o "ProxSpace.tar.xz" "https://siliconbased.us/pm3/install/ProxSpace/ProxSpace_3.11.tar.xz
+curl -LC - --retry 5 --retry-max-time 0 -o "ProxSpace.tar.xz" "https://siliconbased.us/pm3/install/ProxSpace/ProxSpace.tar.xz"
 tar -xvf "./ProxSpace.tar.xz"
 cd ProxSpace
 call runme64.bat -c exit
