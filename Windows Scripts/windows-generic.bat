1ac90c39e686af8d2c5efbb87ff37dc78cbbe548
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2024-08-13 00:39:11 -0700

    Add files via upload
    Checks for spaces in filepath now

--- Windows Scripts/windows-generic.bat
+++ Windows Scripts/windows-generic.bat
@@ -1,7 +1,16 @@
 @echo off
+
+if not "%cd%"=="%cd: =%" goto uhohspaces
+
 curl -LC - --retry 5 --retry-max-time 0 -o "ProxSpace.tar.xz" "https://siliconbased.us/pm3/install/ProxSpace/ProxSpace.tar.xz"
 tar -xvf "./ProxSpace.tar.xz"
 cd ProxSpace
 call runme64.bat -c exit
 curl -L -o "pm3/ProxSpaceScript.sh" "https://siliconbased.us/pm3/install/scripts/ProxSpaceScript-generic.sh"
 ./runme64.bat ./ProxSpaceScript.sh
+exit 0
+
+:uhohspaces
+echo The current folder has spaces in its file path! Scary!!!
+echo Try moving this script to a folder that has no spaces anywhere in its filepath
+echo The current filepath is: %cd%
